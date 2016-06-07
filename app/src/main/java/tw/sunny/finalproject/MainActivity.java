44666d7c04dbce370f8d2d70afa893f466e2d3e1
NagiLin <johnny5581@gmail.com>
2016-06-07 23:00:33 +0800

    fix menu

--- app/src/main/java/tw/sunny/finalproject/MainActivity.java
+++ app/src/main/java/tw/sunny/finalproject/MainActivity.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ProgressBar;
 
 /**
  * Created by lixinting on 2016/4/12.
  */
 public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar = null;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainview);
+        progressBar = (ProgressBar) findViewById(R.id.pgbar);
+        progressBar.setMax(100);
+        progressBar.setProgress(30);
     }
     public void btnShoot(View view){
         Intent intent = new Intent();
@@ -21,9 +26,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void btnSetting(View view){
-        Intent intent = new Intent();
-        intent.setClass(MainActivity.this, SettingMainActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent();
+//        intent.setClass(MainActivity.this, SettingMainActivity.class);
+//        startActivity(intent);
+        progressBar.incrementProgressBy(1);
+        if(progressBar.getProgress() == progressBar.getMax())
+            progressBar.setProgress(0);
     }
     public void btnSearching(View view){
         Intent intent = new Intent();
@@ -35,5 +43,4 @@ public class MainActivity extends AppCompatActivity {
         intent.setClass(MainActivity.this, LocationMainActivity.class);
         startActivity(intent);
     }
-
 }
